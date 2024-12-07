45d577a0dc016216f4cf0bffa7ef6aade46a57ba
dante-rgb-cpu <897510167@qq.com>
2024-12-07 17:25:01 +0800

    Update sheep2.js

--- sheep2.js
+++ sheep2.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const chineseIdiom = '顺手牵羊'; // 这里替换成具体的成语
     const pinyin = 'shunshouqianyang'; // 这里替换成语的拼音
     const explanation = '顺手牵羊是一个汉语成语，最早出自《三十六计》。该成语的意思是顺便拿走别人的东西'; // 这里替换成语的解释
-    const imageFileName = '顺手牵羊.png';
+
 
     document.getElementById('chineseIdiom').textContent = chineseIdiom;
     document.getElementById('chineseIdiom').setAttribute('data-pinyin', pinyin);
@@ -14,6 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
     `;
 
     // 直接设置图片路径
-    const imageUrl = `${imageFileName}`;
+    const imageUrl =sheep.png;
     document.getElementById('idiomImage').src = imageUrl;
 });
